f37a42c89497ced30c019c8193b25840aaf1d4ea
batoan <iambatoan@gmail.com>
2016-01-05 10:44:22 +0700

    Project init

---
+++ SIAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SIAlertView
+//
+//  Created by iSlan on 1/5/16.
+//  Copyright © 2016 iSlan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
